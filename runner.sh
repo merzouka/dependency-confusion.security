34b8c4700f3b9460f4befacb67564066cfcf3786
merzouka <marzoukayouness@gmail.com>
2025-04-21 12:38:04 +0100

    Removed useless logs, using twisted

--- runner.sh
+++ runner.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-which supervisord &> /dev/null
+which twistd &> /dev/null
 if [ $? -ne 0 ]; then
-    echo "Supervisor is not installed, are you in a virtualenv?";
+    echo "Twisted is not installed, are you in a virtualenv?";
     exit 1
 fi
 
-# load configuration
-supervisord -c gen-config/supervisord.conf
-
-# stop running instance
-supervisorctl -c gen-config/supervisord.conf stop devpi-server
-
-# start devpi server
-supervisorctl -c gen-config/supervisord.conf start devpi-server
+twistd -n web --path ./packages/
